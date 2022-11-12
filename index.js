8d2aee5ddbe64b5becb8b796566c26104cd337a4
mollikacomputer <mollikacomputer3@gmail.com>
2022-11-12 22:06:15 +0600

    simple pdf file created done

--- index.js
+++ index.js
@@ -4,3 +4,71 @@ var fs = require("fs");
 
 // Read HTML Template
 var html = fs.readFileSync("index.html", "utf8");
+
+var options = {
+    format: "A4",
+    orientation: "portrait",
+    border: "10mm",
+    header: {
+        height: "45mm",
+        contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+    },
+    footer: {
+        height: "28mm",
+        contents: {
+            first: 'Cover page',
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    }
+};
+// users data
+var users = [
+    {
+        name: "sujon",
+        age: "29",
+      },
+    {
+        name: "Ranjit",
+        age: "42",
+      },
+      {
+        name: "Ramen",
+        age: "45",
+      },
+      {
+        name: "Wallid",
+        age: "48",
+      },
+    {
+      name: "Shyam",
+      age: "26",
+    },
+    {
+      name: "Navjot",
+      age: "26",
+    },
+    {
+      name: "Vitthal",
+      age: "26",
+    },
+  ];
+
+  var document = {
+    html: html,
+    data: {
+      users: users,
+    },
+    path: "./output.pdf",
+    type: "",
+  };
+  // By default a file is created but you could switch between Buffer and Streams by using "buffer" or "stream" respectively.
+
+  pdf
+  .create(document, options)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
